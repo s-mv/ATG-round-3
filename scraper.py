22aa62a3597e9282b0474c004ca1338571e49126
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2023-12-19 17:29:18 +0530

    solution obtained

--- scraper.py
+++ scraper.py
@@ -1,3 +1,24 @@
-def scrape_twitter_link(twitter_link: str):
+
+class TwitterUser():
+    bio : str
+    following, followers : int
+    location : str
+    website : str
+
+    def __init__(self, bio, following, followers, location, website):
+        self.bio = bio
+        self.following = following
+        self.followers = followers
+        self.location = location
+        self.website = website
+
+
+
+def scrape_twitter_links(links: [str]) -> [TwitterUser]:
+    # TODO
+    return {}
+
+
+def get_twitter_user_object(link: str) -> TwitterUser:
     # TODO: return real information
     return {}
